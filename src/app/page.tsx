b53ac62b2753103512d4b0ebf8f1b250e974d61d
Pedro Wilson <pdro.wilson@gmail.com>
2023-08-24 17:04:01 -0300

    Server commponent data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,35 @@
-export default function Home() {
-  return <h1>To do</h1>
+import TodoItem from "@/components/TodoItem";
+import { prisma } from "@/db";
+import Link from "next/link";
+
+function getTodos() {
+  return prisma.todo.findMany()
+}
+
+export default async function Home() {
+  // fake add todo item
+  // await prisma.todo.create({data: {title:"test", complete: false}})
+  
+  const todos =  await getTodos()
+  
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2x1">Todos</h1>
+        <Link 
+          className="boder border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
+          href={"/new"}
+        >
+          New
+        </Link>
+      </header>
+      <ul className="pl-4">
+        {todos.map(todo => {
+          return (
+            <TodoItem key={todo.id} {...todo} />
+          );
+        })}
+      </ul>
+    </>
+  )
 }
