b845ee87ce74f94053e9e797f939697f69efcaaf
Pedro Wilson <pdro.wilson@gmail.com>
2023-08-24 23:34:10 -0300

    create Todo page

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,3 +1,30 @@
-export default function Add() {
-    return <h1>Add</h1>
+import Link from "next/link";
+
+export default function Page() {
+    return (
+        <>
+            <header className="flex justify-between items-center mb-4">
+                <h1 className="text-2x1">New</h1>
+            </header>
+            <form className="flex flex-col gap-2">
+                <input 
+                    type="text"
+                    name="title"
+                    className="border border-slate-400 bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-100"
+                />
+                <div className="flex gap-1 justify-end">
+                    <Link
+                        className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
+                        href="..">
+                        Cancel
+                    </Link>
+                    <button 
+                        className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none" 
+                        type="submit">
+                        Create
+                    </button>
+                </div>
+            </form>
+        </>
+    )
 }
